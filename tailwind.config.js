18a27c83411d8c91ac6951ab1c3214289361c320
unknown <dawidw22@gmail.com>
2023-02-21 17:58:54 +0100

    start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    colors: {
+      veryLightGrayishBlue: "hsl(240, 78%, 98%)",
+      lightGrayishBlue: "hsl(234, 14%, 74%)",
+      grayishBlue: "hsl(233, 13%, 49%)",
+      darkGrayishBlue: "hsl(232, 13%, 33%)",
+      black: "#000",
+      white: "#fff",
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+    },
+    fontFamily: {
+      display: ["Montserrat", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
