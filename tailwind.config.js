bcd56ca42443549828473be8b55d87cdd838d08f
unknown <dawidw22@gmail.com>
2023-02-22 12:41:12 +0100

    finish styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
       lightGrayishBlue: "hsl(234, 14%, 74%)",
       grayishBlue: "hsl(233, 13%, 49%)",
       darkGrayishBlue: "hsl(232, 13%, 33%)",
+      gradientLightBlue: "hsl(236, 72%, 79%)",
+      gradientBlue: "hsl(237, 63%, 64%)",
       black: "#000",
       white: "#fff",
     },
     screens: {
       sm: "480px",
-      md: "768px",
+      md: "750px",
+      lg: "1050px",
     },
     fontFamily: {
       display: ["Montserrat", "sans-serif"],
